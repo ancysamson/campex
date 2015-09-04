e45122c7dab0acafc9a516c9ee4655ea2e12d655
Ancy Samson <ancysamson03@gmail.com>
2015-09-04 14:07:13 +0530

    adds timetable

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -5,4 +5,8 @@ class Batch < ActiveRecord::Base
   def to_s
     self.code
   end
+
+  def current_term
+    Term.where('batch_id = ? AND ? BETWEEN start_date AND end_date', self.id, Date.current).first
+  end
 end
